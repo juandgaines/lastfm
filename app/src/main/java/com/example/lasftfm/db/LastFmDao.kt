e5fc2eda7304e0fc4ec3de9418ccd97b0103ebb0
Juan Gaines <juan.gaines@peiky.com>
2020-03-29 13:07:23 -0500

    feat: Dao functions defined for tracks
    Insert, get and clear functions with livedata

--- app/src/main/java/com/example/lasftfm/db/LastFmDao.kt
+++ app/src/main/java/com/example/lasftfm/db/LastFmDao.kt
@@ -1,5 +1,23 @@
 package com.example.lasftfm.db
 
+import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.lasftfm.network.Track
+
 interface LastFmDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(posts: List<Track>)
+
+    @Query("SELECT * FROM tracks ORDER BY id ASC")
+    fun reposByName(): LiveData<List<Track>>
+
+    @Query("DELETE FROM tracks")
+    fun clearTracks()
+
 
 }
