b1439b39506a9173d433277a7816dc1bdd1dd40d
Juan Gaines <juan.gaines@peiky.com>
2020-03-29 18:40:26 -0500

    feat: Retrofit interceptor added
    Logger for retrofit http requests

--- app/src/main/java/com/example/lasftfm/network/Service.kt
+++ app/src/main/java/com/example/lasftfm/network/Service.kt
@@ -4,10 +4,13 @@ import com.example.lasftfm.BuildConfig
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.logging.Level
 
 
 interface LastFmService {
@@ -28,12 +31,22 @@ interface LastFmService {
 }
 
 object Network {
+    private val logger = HttpLoggingInterceptor()
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logger)
+        .build()
+    init {
+        logger.level = HttpLoggingInterceptor.Level.BASIC
+    }
+
+
     private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
 
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://ws.audioscrobbler.com/")
+        .client(client)
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
 
