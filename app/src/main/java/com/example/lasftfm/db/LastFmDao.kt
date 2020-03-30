194bdb2af2a3a84c5b1e8e8734ec4846ab54a139
Juan Gaines <juan.gaines@peiky.com>
2020-03-30 11:15:02 -0500

    feat: Edit text added to Layouts
    It will help us to assign a query to the filter process

--- app/src/main/java/com/example/lasftfm/db/LastFmDao.kt
+++ app/src/main/java/com/example/lasftfm/db/LastFmDao.kt
@@ -12,8 +12,8 @@ interface LastFmDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(posts: List<Track>)
 
-    @Query("SELECT * FROM tracks")
-    fun listOfTracks():DataSource.Factory<Int, Track>
+    @Query("SELECT * FROM tracks  WHERE (name LIKE :query) ORDER BY name ASC")
+    fun listOfTracks(query:String="%"):DataSource.Factory<Int, Track>
 
     @Query("DELETE FROM tracks")
     fun clearTracks()
@@ -21,8 +21,8 @@ interface LastFmDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertArtist(posts: List<Artist2>)
 
-    @Query("SELECT * FROM artists")
-    fun listOfArtist():DataSource.Factory<Int, Artist2>
+    @Query("SELECT * FROM artists WHERE (name LIKE :query) ORDER BY name ASC")
+    fun listOfArtist(query:String="%"):DataSource.Factory<Int, Artist2>
 
     @Query("DELETE FROM artists")
     fun clearArtist()
